0e928a849eedf644cab45be9b5096d8b6330ef36
ziyixi <ziyixi@mail.ustc.edu.cn>
2022-03-11 16:44:57 -0500

    update slicing functions

--- eara2022/utils/slice.py
+++ eara2022/utils/slice.py
@@ -8,8 +8,10 @@ from typing import List, Tuple
 import numpy as np
 import pyproj
 import xarray as xr
+from obspy.geodetics import locations2degrees
 from scipy.interpolate import RegularGridInterpolator
 from scipy.spatial import KDTree
+from . import generate_tmp_file
 
 
 def model_interp(to_interp_data: xr.DataArray, lons: np.ndarray, lats: np.ndarray, deps: np.ndarray) -> np.ndarray:
@@ -68,5 +70,149 @@ def topo_interp(to_interp_data: xr.DataArray, lons: np.ndarray, lats: np.ndarray
     return grd_interp_result
 
 
-def gmt_lat_as_dist(start: Tuple[float, float], end: Tuple[float, float], a_list: List[float], g_interval: List[float], npts: int = 1001) -> str:
-    pass
+def gmt_lat_as_dist(start: Tuple[float, float], end: Tuple[float, float], a_interval: float, g_interval: float, npts: int = 1001) -> str:
+    """Generate a lebel tmp file for pxc[file name], so we can have evenly sampled ticks in great circle represented as lat/lon
+
+    Args:
+        start (Tuple[float, float]): the start position of the line (lon,lat)
+        end (Tuple[float, float]): the end position of the line (lon,lat)
+        a_interval (float): the interval for first level ticks
+        g_interval (float): the interval for second level ticks
+        npts (int, optional): control the relative error for the returned result
+
+    Returns:
+        str: a tmp file path used for frame
+    """
+    # * init parameters
+    startlon, startlat = start
+    endlon, endlat = end
+    starta = startlat if startlat % a_interval == 0 else (
+        startlat//a_interval+1)*a_interval
+    enda = endlat if endlat % a_interval == 0 else (
+        endlat//a_interval)*a_interval
+    a_list = np.arange(starta, enda, a_interval+enda)
+    # * calculate
+    g = pyproj.Geod(ellps='WGS84')
+    gcarc = locations2degrees(startlat, startlon, endlat, endlon)
+    if startlat > endlat:
+        startlon, endlon = endlon, startlon
+        startlat, endlat = endlat, startlat
+    test_points = np.array(
+        g.npts(startlon, startlat, endlon, endlat, (npts-1)*10+1))
+    tree = KDTree(test_points[:, 1].reshape(test_points.shape[0], -1))
+    # * generate the gmt custome axis file when use lon to plot the cross-section
+    num_list = []
+    type_list = []
+    annote_list = []
+    # before first a
+    for each_lat in np.arange(a_list[0]-g_interval, startlat, -1*g_interval)[::-1]:
+        num_list.append(each_lat)
+        type_list.append("f")
+        annote_list.append("")
+    # start from first a
+    for ia in range(len(a_list)-1):
+        starta = a_list[ia]
+        enda = a_list[ia+1]
+        # first a
+        num_list.append(starta)
+        type_list.append("a")
+        annote_list.append(f"{starta}")
+        for each_g in np.arange(starta+g_interval, enda, g_interval):
+            num_list.append(each_g)
+            type_list.append("f")
+            annote_list.append("")
+    # last a
+    num_list.append(a_list[-1])
+    type_list.append("a")
+    annote_list.append(f"{a_list[-1]}")
+    for each_g in np.arange(a_list[-1]+g_interval, endlat, g_interval):
+        num_list.append(each_g)
+        type_list.append("f")
+        annote_list.append("")
+
+    # convert num_list to actual dist_list
+    num_list = np.array(num_list)
+    _, pos = tree.query(num_list.reshape(len(num_list), -1))
+    dist_list = pos/((npts-1)*10)*gcarc
+
+    # write lists to a temp file
+    tmp = generate_tmp_file()
+    with open(tmp, 'w') as f:
+        for index in range(len(num_list)):
+            f.write(
+                f"{dist_list[index]}  {type_list[index]}  {annote_list[index]} \n")
+    return tmp
+
+
+def gmt_lon_as_dist(start: Tuple[float, float], end: Tuple[float, float], a_interval: float, g_interval: float, npts: int = 1001) -> str:
+    """Generate a lebel tmp file for pxc[file name], so we can have evenly sampled ticks in great circle represented as lat/lon
+
+    Args:
+        start (Tuple[float, float]): the start position of the line (lon,lat)
+        end (Tuple[float, float]): the end position of the line (lon,lat)
+        a_interval (float): the interval for first level ticks
+        g_interval (float): the interval for second level ticks
+        npts (int, optional): control the relative error for the returned result
+
+    Returns:
+        str: a tmp file path used for frame
+    """
+    # * init parameters
+    startlon, startlat = start
+    endlon, endlat = end
+    starta = startlon if startlon % a_interval == 0 else (
+        startlon//a_interval+1)*a_interval
+    enda = endlon if endlon % a_interval == 0 else (
+        endlon//a_interval)*a_interval
+    a_list = np.arange(starta, enda, a_interval+enda)
+    # * calculate
+    g = pyproj.Geod(ellps='WGS84')
+    gcarc = locations2degrees(startlat, startlon, endlat, endlon)
+    if startlon > endlon:
+        startlon, endlon = endlon, startlon
+        startlat, endlat = endlat, startlat
+    test_points = np.array(
+        g.npts(startlon, startlat, endlon, endlat, (npts-1)*10+1))
+    tree = KDTree(test_points[:, 0].reshape(test_points.shape[0], -1))
+    # * generate the gmt custome axis file when use lon to plot the cross-section
+    num_list = []
+    type_list = []
+    annote_list = []
+    # before first a
+    for each_lon in np.arange(a_list[0]-g_interval, startlon, -1*g_interval)[::-1]:
+        num_list.append(each_lon)
+        type_list.append("f")
+        annote_list.append("")
+    # start from first a
+    for ia in range(len(a_list)-1):
+        starta = a_list[ia]
+        enda = a_list[ia+1]
+        # first a
+        num_list.append(starta)
+        type_list.append("a")
+        annote_list.append(f"{starta}")
+        for each_g in np.arange(starta+g_interval, enda, g_interval):
+            num_list.append(each_g)
+            type_list.append("f")
+            annote_list.append("")
+    # last a
+    num_list.append(a_list[-1])
+    type_list.append("a")
+    annote_list.append(f"{a_list[-1]}")
+    for each_g in np.arange(a_list[-1]+g_interval, endlon, g_interval):
+        num_list.append(each_g)
+        type_list.append("f")
+        annote_list.append("")
+
+    # convert num_list to actual dist_list
+    num_list = np.array(num_list)
+    _, pos = tree.query(num_list.reshape(len(num_list), -1))
+    dist_list = pos/((npts-1)*10)*gcarc
+
+    # write lists to a temp file
+    tmp = generate_tmp_file()
+    with open(tmp, 'w') as f:
+        for index in range(len(num_list)):
+            f.write(
+                f"{dist_list[index]}  {type_list[index]}  {annote_list[index]} \n")
+    return tmp
