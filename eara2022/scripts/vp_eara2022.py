cd080d693d1fe3defbf6c303c44373a8da9d0247
Ziyi Xi <ziyixi@mail.ustc.edu.cn>
2023-05-16 16:55:30 -0400

    Update supplementary figures

--- eara2022/scripts/vp_eara2022.py
+++ eara2022/scripts/vp_eara2022.py
@@ -10,17 +10,25 @@ import numpy as np
 from .vpvs_base import plot_base
 
 conf = {
-    'model_type': "vp",
-    'depths': np.arange(100, 1000, 100),
-    'cpt_series': "-0.06/0.06/0.01",
-    'cpt_reverse': False,
-    'save_name': "vp_eara2022",
-    'colorbar_content': "@~d@~lnVp(%)",
-    'ref': 'eara2022'
+    "model_type": "vp",
+    "depths": np.arange(100, 1000, 100),
+    "cpt_series": "-0.06/0.06/0.01",
+    "cpt_reverse": False,
+    "save_name": "vp_eara2022",
+    "colorbar_content": "@[\delta@[ln@[V_P@[",
+    "ref": "eara2022",
 }
 
-main = partial(plot_base, model_type=conf['model_type'], depths=conf['depths'], cpt_series=conf['cpt_series'],
-               cpt_reverse=conf['cpt_reverse'], save_name=conf['save_name'], colorbar_content=conf['colorbar_content'], ref=conf['ref'])
+main = partial(
+    plot_base,
+    model_type=conf["model_type"],
+    depths=conf["depths"],
+    cpt_series=conf["cpt_series"],
+    cpt_reverse=conf["cpt_reverse"],
+    save_name=conf["save_name"],
+    colorbar_content=conf["colorbar_content"],
+    ref=conf["ref"],
+)
 
 if __name__ == "__main__":
     main()
